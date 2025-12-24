0ea739c1f8ee395135ab1d7f63f9a34c4d7eeef4
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-12-24 09:02:33 +0900

    feat : 로그인 페이지 UI 구현

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,3 +1,71 @@
+import logo from "@/assets/logo-transparent.png";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { Link } from "react-router";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+      <div className="w-full max-w-md rounded-lg bg-white p-8 shadow">
+        <div className="flex items-center justify-center gap-4">
+          <img src={logo} alt="Formation-Lab 로고" className="h-12" />
+          <h1 className="text-brand-green text-2xl font-bold tracking-wide sm:text-3xl">
+            Formation-Lab
+          </h1>
+        </div>
+
+        <div className="mt-6">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="email"
+                className="text-[14px] text-[rgba(51,65,85,1)]"
+              >
+                이메일
+              </Label>
+              <Input
+                className="py-5"
+                type="email"
+                placeholder="이메일 주소를 입력해 주세요"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="password"
+                className="text-[14px] text-[rgba(51,65,85,1)]"
+              >
+                비밀번호
+              </Label>
+              <Input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="py-5"
+                type="password"
+                placeholder="비밀번호를 입력해 주세요"
+              />
+              <p className="text-muted-foreground text-sm">6자리 이상</p>
+            </div>
+
+            <Button className="cursor-pointer rounded-full py-5">로그인</Button>
+
+            <div className="text-center">
+              <Link
+                className="text-muted-foreground hover:underline"
+                to={"/signup"}
+              >
+                계정이 없으신가요? 회원가입
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
